37e5fadc8ce5c52f480c4288a36b664190e4a4ee
Heycoming <lubing@umich.edu>
2025-12-06 00:42:31 -0500

    change: script.js

--- js/script.js
+++ js/script.js
@@ -2,13 +2,7 @@ const addNumbers = (first, second) => {
   return first + second;
 }
 
-if (typeof module !== 'undefined') {
-  module.exports = {
-    addNumbers
-  }
-}
-
-document.querySelector('#add').addEventListener('click', () => {
+document.querySelector('#addNum').addEventListener('click', () => {
   const firstNumber = document.querySelector('#firstNumber').value;
   const secondNumber = document.querySelector('#secondNumber').value;
   const result = addNumbers(Number(firstNumber), Number(secondNumber));
