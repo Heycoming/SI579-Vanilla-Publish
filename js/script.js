bbe989e7ad6caa8fabb51280b436e9afdbdca640
Heycoming <lubing@umich.edu>
2025-12-10 23:13:57 -0500

    fix

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ document.querySelector('#addNum').addEventListener('click', () => {
   const firstNumber = document.querySelector('#firstNumber').value;
   const secondNumber = document.querySelector('#secondNumber').value;
   const result = addNumbers(Number(firstNumber), Number(secondNumber));
-  alert(`The result is: ${result}`);
+  document.querySelector('#display').textContent = result;
 });
 
 if (typeof module !== "undefined") {
