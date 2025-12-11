8b8cada31c4918b2cbdb77e91e11714670a24af2
Heycoming <lubing@umich.edu>
2025-12-10 23:06:57 -0500

    fix scrip.js

--- js/script.js
+++ js/script.js
@@ -8,3 +8,7 @@ document.querySelector('#addNum').addEventListener('click', () => {
   const result = addNumbers(Number(firstNumber), Number(secondNumber));
   alert(`The result is: ${result}`);
 });
+
+if (typeof module !== "undefined") {
+  module.exports = { addNumbers };
+}
